4d7ea2c4c5b698b76b7ece826186eccf1b525b60
PradhyumnaVA <137704414+PradhyumnaVA@users.noreply.github.com>
2024-12-20 15:56:57 +0530

    Update tb_dff.v

--- tb_dff.v
+++ tb_dff.v
@@ -4,16 +4,20 @@ reg d;
 reg reset, clk;
 wire q, q_b;
 
-dff DUT (.d(d), .reset(reset), .clk(clk), .q(q), .q_b(q_b));
+d_ff DUT (.d(d), .reset(reset), .clk(clk), .q(q), .q_b(q_b));
+
 
 initial begin
 	clk=1;
+	d = 0;
 	reset=1;
+	#10;
 	reset=0;
 	#100 $finish;
 end
 
 always #5 clk=~clk;
+always #7 d=~d;
 
 initial begin
 	$dumpfile("dump.vcd");
